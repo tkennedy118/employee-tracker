f0e0ed7b3fc2df181914c59d446220f5a3f3bfa9
Tyler Kennedy <tkennedy118@gmail.com>
2020-03-25 20:49:19 -0500

    add functions to add department, employee, and role

--- db/companyDB.js
+++ db/companyDB.js
@@ -14,6 +14,30 @@ class CompanyDB {
         return result;
     }
 
+    async addDepartment(name) {
+        const query = `INSERT INTO department (name) VALUES ('${name}')`;
+        const result = await this.connection.query(query);
+
+        if (!result) throw new Error("Could not add department.");
+        return result;
+    }
+
+    async addRole(title, salary, department_id) {
+        const query = `INSERT INTO role (title, salary, department_id) VALUES ('${title}', ${salary}, ${department_id})`;
+        const result = await this.connection.query(query);
+
+        if (!result) throw new Error("Could not add role.");
+        return result;
+    }
+
+    async addEmployee(first_name, last_name, role_id, manager_id) {
+        const query = `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ('${first_name}', '${last_name}', ${role_id}, ${manager_id})`;
+        const result = await this.connection.query(query);
+
+        if(!result) throw new Error("Could not add employee.");
+        return result;
+    }
+
     disconnect() {
         this.connection.end();
     }
